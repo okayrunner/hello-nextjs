8bf89462c9e4ce6f3db2879e695a9ccf44abc768
Arik Cohen <arik@codecademy.com>
2021-03-18 13:01:15 -0400

    wow

--- pages/todos.js
+++ pages/todos.js
@@ -3,11 +3,12 @@ import useSWR from 'swr'
 const fetcher = url => fetch(url).then(r => r.json())
 
 function Todos({ todos }) {
+    // client-side fetching (with SSG data initially!)
     const { data,error } = useSWR('https://mockend.com/okayrunner/hello-nextjs-data/todos?limit=5', fetcher, { initialData: todos })
 
     if (error) return <div>failed to load</div>
     if (!data) return <div>loading...</div>
-    
+
     return (
       <ul>
       {data.map((todo) => (
@@ -17,7 +18,8 @@ function Todos({ todos }) {
     )
   }
 
-export async function getServerSideProps() {
+// SSG
+export async function getStaticProps() {
   // Fetch data from external API
   const res = await fetch(`https://mockend.com/okayrunner/hello-nextjs-data/todos`)
   const todos = await res.json()
